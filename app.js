e438be3ebb8afa83fbc6a69cc49d34fe30f4ddf4
Mohitk391 <mohitkpatel391@gmail.com>
2021-08-19 00:50:53 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-/*console.log(`hello again.`);
+var btnTranslate = document.querySelector("#btn-translate");
+var userInput = document.querySelector("#input-user");
+var userOutput = document.querySelector("#output-text");
 
-var name = prompt("please enter your name");
-console.log(name);
-alert(`entered name is: ${name}`);
-*/
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function convertedURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    alert("something wrong with the server, please try again after some time.");
+    return "error is: "+error;
+}
+
+function clickEventHandler(){
+    fetch(convertedURL(userInput.value))
+                        .then(response => response.json())
+                        .then(json =>{
+                            var translatedText = json.contents.translated;
+                            userOutput.innerText = translatedText;})
+                        .catch(errorHandler);
+
+    
+}
+
+
+btnTranslate.addEventListener("click", clickEventHandler);
+
+    
 
